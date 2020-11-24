88f0ec0c6661ae814fbd33f626e8846d94afe9ce
Sergey Volchkov <debfantasys@gmail.com>
2020-11-25 00:56:11 +0700

    minor update. update header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,21 +4,23 @@ class Header extends React.Component {
 
     render() {
         return (
-            <header className="header container" id="header">
-                <a className="header__logo" href="/#">volkodav</a>
-                <ul className="header__menu">
-                    <li>
-                        <a href="/#" className="menu__item">Блог</a>
-                    </li>
-                    <li>
-                        <a href="/#" className="menu__item">Контакты</a>
-                    </li>
-                    <li>
-                        <button type="button" className="menu-btn__appointment ">
-                            Записаться
-                        </button>
-                    </li>
-                </ul>
+            <header className="container-fluid">
+                <div className="header container">
+                    <a className="header__logo" href="/#">volkodav</a>
+                    <ul className="header__menu">
+                        <li>
+                            <a href="/#" className="menu__item">Блог</a>
+                        </li>
+                        <li>
+                            <a href="/#" className="menu__item">Контакты</a>
+                        </li>
+                        <li>
+                            <button type="button" className="menu-btn__appointment ">
+                                Записаться
+                            </button>
+                        </li>
+                    </ul>
+                </div>
             </header>
         )
     }
