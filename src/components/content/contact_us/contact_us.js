f4d410dc67af74779f7bbd27d72c7961eb09d86d
Sergey Volchkov <debfantasys@gmail.com>
2020-11-25 23:21:15 +0700

    added modal for form

--- src/components/content/contact_us/contact_us.js
+++ src/components/content/contact_us/contact_us.js
@@ -30,36 +30,78 @@ class ContactUs extends React.Component {
 }
 
 class Form extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            modal: false,
+        }
+
+        this.onClick = this.onClick.bind(this);
+    }
+
+    onClick = (event) => {
+        this.setState({
+            modal: !this.state.modal,
+        })
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <form action="/#" method="post" className="contact-us__form">
-                <label htmlFor="name">
-                    Имя
-                    <input type="text" name="name"/>
-                </label>
-                <label htmlFor="phone">
-                    Номер телефона
-                    <input type="phone" name="phone"/>
-                </label>
-                <label htmlFor="email">
-                    Почта
-                    <input type="email" name="email"/>
-                </label>
+            <div>
+                {this.state.modal ? <ModalInfo onClick={this.onClick}/> : null}
+                <form onSubmit={this.onClick} method="post" className="contact-us__form">
+                    <label htmlFor="name">
+                        Имя
+                        <input type="text" name="name" required/>
+                    </label>
+                    <label htmlFor="phone">
+                        Номер телефона
+                        <input type="phone" name="phone" required/>
+                    </label>
+                    <label htmlFor="email">
+                        Почта
+                        <input type="email" name="email" required/>
+                    </label>
 
-                <button className="contact-us__btn">
-                    Отправить
-                </button>
+                    <button className="contact-us__btn" >
+                        Отправить
+                    </button>
 
-                <label htmlFor="newsletter">
-                    <input type="checkbox" name="newsletter"/>
-                    Получать новостную рассылку на электронную почту
-                </label>
-                <label htmlFor="eula">
-                    <input type="checkbox" name="eula"/>
-                    Согласен с условиями обработки персональных данных
-                </label>
-            </form>
+                    <label htmlFor="newsletter">
+                        <input type="checkbox" name="newsletter"/>
+                        Получать новостную рассылку на электронную почту
+                    </label>
+                    <label htmlFor="eula">
+                        <input type="checkbox" name="eula" required/>
+                        Согласен с условиями обработки персональных данных
+                    </label>
+                </form>
+            </div>
         );
+    }
+}
+
+class ModalInfo extends React.Component {
+    render() {
+        return (
+            <div className="modal-bg">
+                <div className="modal">
+                    <h3 className="modal__title">
+                        Нет слов, одним эмоции, если вы тут что-то заполнили<br /> 😻
+                    </h3>
+                    <p className="modal__text">
+                        Отправить форму можно будет позже))
+                        Если её хоть кто-нибудь нажмёт...
+                    </p>
+                    <button onClick={this.props.onClick}>
+                        Буду ждать
+                    </button>
+                </div>
+            </div>
+        )
     }
 }
 
