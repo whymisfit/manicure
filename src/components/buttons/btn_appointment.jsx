23ea23279347860d0c00e88ac86eaa99621756bf
Sergey Volchkov <debfantasys@gmail.com>
2020-11-25 23:09:19 +0700

    added modal

--- src/components/buttons/btn_appointment.jsx
+++ src/components/buttons/btn_appointment.jsx
@@ -5,20 +5,53 @@ class BtnAppointment extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            modal: false,
+        }
+
         this.onClick = this.onClick.bind(this);
     }
 
     onClick = () => {
-        alert("привет");
+        this.setState({
+            modal: !this.state.modal,
+        })
     }
 
     render() {
         const _style = this.props.style;
 
         return (
-            <button className="btn-appointment" style={_style} onClick={this.onClick}>
-                Записаться
-            </button>
+            <div>
+                <button className="btn-appointment" style={_style} onClick={this.onClick}>
+                    Записаться
+                </button>
+                { this.state.modal ? <ModalInfo onClick={this.onClick}/> : null}
+            </div>
+        )
+    }
+}
+
+class ModalInfo extends React.Component {
+    render() {
+        return (
+            <div className="modal-bg">
+                <div className="modal">
+                    <h3 className="modal__title">
+                        Спасибо что хотели записаться ко мне! <br /> 🥰
+                    </h3>
+                    <p className="modal__text">
+                        Если шо, можно писать в <a href="https://vk.com/sereganyebatkaktaktovrotkompot">ВК</a> или
+                        на почту <span><b>seregabigjopa@yandex.ru</b></span>
+                    </p>
+                    <p className="modal__desc">
+                        Но пока-что я не работаю, пока-что ...
+                    </p>
+                    <button onClick={this.props.onClick}>
+                        Понял!
+                    </button>
+                </div>
+            </div>
         )
     }
 }
